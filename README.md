a604b29cad8fe5b973a327b2445ddd41a8fecea9
Jon Cuthbert <jon@cuth.net>
2015-04-02 11:36:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+transition-rotator
+==================
+
+This just relies on CSS for any transition between items. Every item will be able to be identified in CSS as next, previous or active.
+
+Browsers that don't support CSS transitions still see the end result. [caniuse](http://caniuse.com/#feat=css-transitions)
+
+ES5 polyfills needed for:
+* Array.isArray
+* Array.forEach
+* Array.map
+* Object.keys
+
+Dependencies: none
+
+Create a TransitionRotator
+--------------------------
+
+```js
+var arrayOfItems = Array.prototype.slice.call(document.querySelectorAll('.items'), 0);
+
+// this wouldn't be necessary since all these options are the defaults
+var options = {
+    next: 'next',
+    previous: 'previous',
+    active: 'active',
+    attribute: 'data-position'
+};
+
+var rotator = new TransitionRotator(arrayOfItems, options);
+```
+
+Change the active index
+-----------------------
+
+```js
+rotator.activate(0);
+```
---
+++ transition-rotator.js
@@ -0,0 +1,89 @@
+/*  transition-rotator
+ *  version: 1.0.0
+ *  https://stash.c2mpg.com:8443/projects/C2/repos/transition-rotator
+ *  @preserve
+ */
+
+/* exported TransitionRotator */
+
+var TransitionRotator = (function () {
+    'use strict';
+
+    var defaults = {
+        next: 'next',
+        previous: 'previous',
+        active: 'active',
+        attribute: 'data-position'
+    };
+
+    function Item(el, attribute) {
+        this.el = el;
+        this.attribute = attribute;
+        this.position = el.getAttribute(attribute) || '';
+    }
+
+    Item.prototype.setPosition = function (position) {
+        if (this.position === position) return;
+
+        this.el.setAttribute(this.attribute, position);
+        this.position = position;
+    };
+
+    // context should be an instance of Rotator
+    var getPosition = function (itemIndex) {
+        if (typeof this.activeIndex !== 'number') {
+            return this.opts.next;
+        }
+        if (itemIndex < this.activeIndex) {
+            return this.opts.previous;
+        }
+        if (itemIndex === this.activeIndex) {
+            return this.opts.active;
+        }
+        return this.opts.next;
+    };
+
+    // context should be an instance of Rotator
+    var getInitialIndex = function () {
+        for (var x = 0, xlen = this.items.length; x < xlen; x += 1) {
+            if (this.items[x].position === this.opts.active) return x;
+        }
+        return null;
+    };
+
+    function Rotator(set, options) {
+
+        if (!Array.isArray(set)) {
+            return new Error('The first parameter must be an array of elements.');
+        }
+
+        this.opts = {};
+        if (typeof options === 'object') {
+            Object.keys(defaults).forEach(function (key) {
+                this.opts[key] = (typeof options[key] === 'undefined') ? defaults[key] : options[key];
+            }, this);
+        } else {
+            Object.keys(defaults).forEach(function (key) {
+                this.opts[key] = defaults[key];
+            }, this);
+        }
+
+        this.items = set.map(function (el) {
+            return new Item(el, this.opts.attribute);
+        }, this);
+
+        this.activeIndex = getInitialIndex.call(this);
+    }
+
+    Rotator.prototype.activate = function (index) {
+        if (this.activeIndex === index) return;
+
+        this.activeIndex = index;
+
+        this.items.forEach(function (item, i) {
+            item.setPosition(getPosition.call(this, i));
+        }, this);
+    };
+
+    return Rotator;
+}());
